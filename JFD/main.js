a5d61ed2191964627206c6c5deba1c6a70b67df4
arnurshulbaev <arnurshulbaev@mail.ru>
2017-12-25 23:02:06 +0600

    JFD

---
+++ JFD/main.js
@@ -0,0 +1,63 @@
+var i=0;
+var i2=0;
+var i3=0;
+var img = document.getElementById('aus');
+auss();
+function auss() {
+
+	var n;
+	n = Math.floor(Math.random()*3+1);	
+  $("#aus").css("left","1px");
+  $("#aus").css("top","1px");
+	switch(n){
+		case 1: {
+			img.src = "img/black.png"; break;
+		}
+		case 2: {
+			img.src = "img/write.png"; break;
+		}
+		case 3: {
+			img.src = "img/wolf.png"; break;
+		}
+	}
+}
+
+ 	$(function(){
+  $("#aus").draggable();
+	    $("#korzina1").droppable({
+  	activeClass: "active",
+  	hoverClass: "hover",
+  	drop: function(){
+      if (img.src=="file:///C:/Documents/GitHub/Shulbaev.github.io/arnurshulbaev.github.io/JFD/img/write.png") {
+  		i=i+1;}
+      else{ if (img.src=="file:///C:/Documents/GitHub/Shulbaev.github.io/arnurshulbaev.github.io/JFD/img/wolf.png"){
+      i=0;
+      }}
+  	  $("#score1").html(""+i+"");
+      auss();
+  	}
+	});
+	});
+  $("#korzina2").droppable({
+    activeClass: "active",
+    hoverClass: "hover",
+    drop: function(){
+      if (img.src=="file:///C:/Documents/GitHub/Shulbaev.github.io/arnurshulbaev.github.io/JFD/img/wolf.png") {
+      i2=i2+1;}
+      $("#score2").html(""+i2+"");
+      auss();
+    }
+  });
+  $("#korzina3").droppable({
+    activeClass: "active",
+    hoverClass: "hover",
+    drop: function(){
+      if (img.src=="file:///C:/Documents/GitHub/Shulbaev.github.io/arnurshulbaev.github.io/JFD/img/black.png") {
+      i3=i3+1;}
+      else{ if (img.src=="file:///C:/Documents/GitHub/Shulbaev.github.io/arnurshulbaev.github.io/JFD/img/wolf.png"){
+      i3=0;
+      }}
+      $("#score3").html(""+i3+"");
+      auss();
+    }
+  });
